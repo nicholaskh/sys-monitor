c69533802eb2f9d57238785a1df11501676d3964
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-05-22 13:41:39 +0800

    currently 100 is enough

--- controllers/StatsController.php
+++ controllers/StatsController.php
@@ -6,7 +6,7 @@ use app\models\SysStats;
 
 class StatsController extends BaseController {
 
-    const SHOW_ELAPSED_STATS_NUMBER = 1000;
+    const SHOW_ELAPSED_STATS_NUMBER = 100;
     const ELAPSED_STATS_INTERVAL = 300;
 
     public $title = '数据统计';
