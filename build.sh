21a029885adb0cd7ea124d866c50ac107f2a4809
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-06-08 16:35:56 +0800

    exclude the config file

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -a --delete --exclude='.git/' --exclude='.gitignore' . sre@10.77.152.10:/var/wd/ffan-sys-monitor
+rsync -a --delete --exclude='.git/' --exclude='.gitignore' --exclude='config/web.php' . sre@10.77.152.10:/var/wd/ffan-sys-monitor
