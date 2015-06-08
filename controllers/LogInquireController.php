2e636f43b320a439e847775c0d24a7c8081d7dcf
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-06-08 13:56:09 +0800

    search and sort for elapsed stats

--- controllers/LogInquireController.php
+++ controllers/LogInquireController.php
@@ -11,11 +11,31 @@ class LogInquireController extends BaseController {
     public $title = '日志查询';
 
     public function actionApp() {
+        $query = Log::find();
+        if ($startDate = \Yii::$app->request->get('startdate')) {
+            $sts = strtotime($startDate);
+        } else {
+            $sts = 0;
+        }
+        if ($endDate = \Yii::$app->request->get('enddate')) {
+            $ets = strtotime($endDate) + 86399;
+        } else {
+            $ets = time();
+        }
+        $query = $query->andWhere(['between', 'ts', $sts, $ets]);
         $provider = new ActiveDataProvider([
-            'query' => Log::find(),
+            'query' => $query,
             'sort' => [
                 'defaultOrder' => [
                     'ts' => SORT_DESC,
+                ],
+                'attributes' => [
+                    'ts' => [
+                        'label' => '日期',
+                    ],
+                    'log' => [
+                        'label' => '日志',
+                    ],
                 ],
             ],
             'pagination' => [
@@ -25,6 +45,8 @@ class LogInquireController extends BaseController {
 
         return $this->render('app', [
             'dataProvider' => $provider,
+            'startDate' => $startDate,
+            'endDate' => $endDate,
         ]);
     }
 
--- views/log-inquire/app.php
+++ views/log-inquire/app.php
@@ -9,16 +9,32 @@ tr{width: 100%;}
 td{vertical-align:top;word-break:break-all;}
 ul, li{list-style-type:none;}
 li{display: inline}
+.search{margin-bottom: 10px;}
+.search label{margin-left: 10px;}
+.gray{color: #999}
 </style>
-<table>
-<tr>
-    <th>时间</th>
-    <th>日志</th>
-</tr>
+
+<div class="search">
+<form method="GET" action="">
+    <label>起始日期<span class="gray">(YYYY-MM-DD):</span></label>
+    <input name="startdate" size=10 <?= ($startDate?"value=$startDate":'')?> />
+    <label>结束日期:</label>
+    <input name="enddate" size=10 <?= ($endDate?"value=$endDate":'')?> />
+    <input type="submit" value="查询" />
+</form>
+</div>
 <?php
 echo ListView::widget([
     'dataProvider' => $dataProvider,
+    'layout' => '{summary}<table>{sorter}{items}</table>{pager}',
     'itemView' => '_log',
+    'sorter' => [
+        'options' => [
+            'tag' => 'tr',
+            'item' => function($item, $index) {
+                return "<th>{$item}</th>";
+            },
+        ],
+    ]
 ]);
 ?>
-</table>
